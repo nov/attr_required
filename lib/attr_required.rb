765899b14884238c67a940a3ebb7c2d3b4a8b781
nov matake <nov@matake.jp>
2010-12-04 14:27:40 +0900

    Finally I got what "extend" does. No need "class << self" in includable module.

--- lib/attr_required.rb
+++ lib/attr_required.rb
@@ -2,56 +2,63 @@ module AttrRequired
   class AttrMissing < StandardError; end
 
   def self.included(klass)
-    class << klass
+    klass.send :include, Includable
+    extended(klass)
+  end
 
-      def attr_required(*keys)
-        @required_attributes ||= []
-        @required_attributes += Array(keys)
-        attr_accessor *keys
+  def self.extended(klass)
+    klass.send :extend, Extendable
+  end
+
+  module Extendable
+    def attr_required(*keys)
+      @required_attributes ||= []
+      @required_attributes += Array(keys)
+      attr_accessor *keys
+    end
+
+    def attr_required?(key)
+      required_attributes.include?(key)
+    end
+
+    def required_attributes
+      Array(@required_attributes)
+    end
+
+    def inherited(subclass)
+      unless required_attributes.empty?
+        subclass.attr_required *required_attributes
       end
+    end
+  end
 
-      def attr_required?(key)
-        required_attributes.include?(key)
+  module Includable
+    def required_attributes
+      self.class.required_attributes
+    end
+
+    def attr_required?(key)
+      self.class.attr_required? key
+    end
+
+    def attr_missing?
+      !attr_missing.empty?
+    end
+
+    def attr_missing!
+      if attr_missing?
+        raise AttrMissing.new("'#{attr_missing.join('\', \'')}' required.")
       end
+    end
 
-      def required_attributes
-        Array(@required_attributes)
-      end
-
-      def inherited(subclass)
-        unless required_attributes.empty?
-          subclass.attr_required *required_attributes
+    def attr_missing
+      required_attributes.select do |key|
+        value = send(key)
+        if value.respond_to?(:empty?)
+          value.empty?
+        else
+          value.nil?
         end
-      end
-
-    end
-  end
-
-  def required_attributes
-    self.class.required_attributes
-  end
-
-  def attr_required?(key)
-    self.class.attr_required? key
-  end
-
-  def attr_missing?
-    !attr_missing.empty?
-  end
-
-  def attr_missing!
-    if attr_missing?
-      raise AttrMissing.new("'#{attr_missing.join('\', \'')}' required.")
-    end
-  end
-
-  def attr_missing
-    required_attributes.select do |key|
-      value = send(key)
-      if value.respond_to?(:empty?)
-        value.empty?
-      else
-        value.nil?
       end
     end
   end
